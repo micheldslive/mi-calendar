54a515ceb1a733197cf66f15504acff528c8bd95
Michel Domingos <michel.silva@penseavanti.com.br>
2022-08-21 22:06:32 -0300

    fix: changes in pages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,26 @@
-import type { NextPage } from 'next'
+import { EventsCalendar } from '@/components/EventsCalendar'
+import { Header } from '@/components/Header'
+import Meta from '@/layout'
+import Main from '@/template'
 
-const Home: NextPage = () => {
+const Index = () => {
   return (
-    <div>
-    </div>
+    <Main
+      meta={
+        <Meta
+          title="mi-calendar"
+          description="Mi-Calendar front-end challange"
+        />
+      }
+    >
+      <main className="flex w-full flex-col bg-primary scrollbar">
+        <Header />
+        <div className="flex w-full flex-1 flex-row bg-primary">
+          <EventsCalendar />
+        </div>
+      </main>
+    </Main>
   )
 }
 
-export default Home
+export default Index
