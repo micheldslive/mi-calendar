c94199b8237dbcb181f95ade8d501e258ee3741f
Michel Domingos <michel.silva@penseavanti.com.br>
2022-08-22 07:37:28 -0300

    fix: fixing scheduler problem

--- src/components/ScheduleTable/index.tsx
+++ src/components/ScheduleTable/index.tsx
@@ -2,16 +2,19 @@ import { ViewState } from '@devexpress/dx-react-scheduler'
 import {
   Appointments,
   DayView,
-  Resources,
-  Scheduler
+  Resources
 } from '@devexpress/dx-react-scheduler-material-ui'
 import Paper from '@mui/material/Paper'
+import Scheduler from '@/devexpress/dx-react-scheduler-material-ui'
 
 import { useStateContext } from '@/context/StateContext'
 import { ISchedulerView } from '@/core/types'
 import { resources } from '@/mocks/dataResouces'
 
-export const ScheduleView = ({ events, currentDate }: ISchedulerView) => {
+export const ScheduleView: React.FC<ISchedulerView> = ({
+  events,
+  currentDate
+}) => {
   const { search } = useStateContext()
 
   const filtered = events.filter(({ title }) =>
---
+++ src/devexpress/dx-react-scheduler-material-ui.tsx
@@ -0,0 +1,2 @@
+import { Scheduler } from '@devexpress/dx-react-scheduler-material-ui'
+export default Scheduler as any
